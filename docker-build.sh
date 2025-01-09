7d9f3efe5a5c760b99b85bc1fa010b61dd3cab15
Lia Petronio <lpetroni@broadinstitute.org>
2025-01-09 10:01:40 -0500

    updated docker files working with aws on dev url

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
-ENV='sandbox'
+ENV='production'
 
 #!/usr/bin/env bash
 # cd vue
@@ -8,9 +8,10 @@ npm run build
 #cd ../
 
  LOCAL_IMAGE_NAME=prism/website:$ENV
-# ECR_REPO=207675869076.dkr.ecr.us-east-1.amazonaws.com/prism-portal-$ENV
+#  ECR_REPO=207675869076.dkr.ecr.us-east-1.amazonaws.com/prism-vue-website-$ENV // this will be for when we have dev/sandbox. need to setup new repo on aws
+ ECR_REPO=207675869076.dkr.ecr.us-east-1.amazonaws.com/prism-vue-website # this is copied from aws repo
 
-#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 207675869076.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 207675869076.dkr.ecr.us-east-1.amazonaws.com
 docker build --platform=linux/amd64 -t $LOCAL_IMAGE_NAME .
-#docker tag $LOCAL_IMAGE_NAME $ECR_REPO
-# docker push $ECR_REPO
+docker tag $LOCAL_IMAGE_NAME $ECR_REPO
+docker push $ECR_REPO
