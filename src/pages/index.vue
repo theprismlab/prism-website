0036162f34864fba2973aa087f5db0a418873906
Lia Petronio <lpetroni@broadinstitute.org>
2025-01-13 13:22:06 -0500

    home page section 2 card layout with text and buttons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-layout>
-    <span class="bg"></span>
-      <v-container fluid  id="hero-section" class="container-custom" :style="{paddingTop: heroPadding, height: heroHeight, zIndex: 1}">
+  <div>
+    <div class="bg-hero" style="position: relative;">
+      <v-container fluid class="container-custom" :style="{paddingTop: heroPadding, height: heroHeight, zIndex: 1}">
         <v-row>
           <v-col cols="12" xs="12" sm="12" md="10" lg="8" xl="6">
             <h1>Accelerate the understanding of cancer treatments </h1>
@@ -9,7 +9,79 @@
           </v-col>
         </v-row>
       </v-container>
-  </v-layout>
+    </div>
+   
+    <div class="bg-gradient" style="position: relative;">
+      <v-container fluid class="container-custom">
+        <h2 class="light">PRISM multiplexed viability screening with over 900 genomically characterized cell lines.</h2>
+        <v-row>
+            <v-col cols="auto" xs="12" sm="12" md="3" lg="3" xl="3">
+            <v-card :style="{ height: cardHeight + 'px' }">
+              <v-card-title> 900+ cancer cell lines barcoded and pooled</v-card-title>
+              <v-card-text>
+              Hematopoietic and solid tumor, covering 80 cancer subtypes
+              </v-card-text>
+              <v-card-actions>
+                <v-btn class="text-subtitle-1" color="primary" variant="outlined" rounded>
+                  Learn more 
+                  <v-icon>mdi-arrow-right-thin</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+            </v-col>
+
+            <v-col cols="auto" xs="12" sm="12" md="3" lg="3" xl="3">
+            <v-card :style="{ height: cardHeight + 'px' }">
+              <v-card-title>Viability profile and genomic characterization</v-card-title>
+              <v-card-text>
+              Generate univariate and multivariate predictive models comparing PRISM viability profile with Dependency Map features
+              </v-card-text>
+              <v-card-actions>
+                <v-btn class="text-subtitle-1" color="primary" variant="outlined" rounded>
+                  Learn more 
+                  <v-icon>mdi-arrow-right-thin</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+            </v-col>
+
+            <v-col cols="auto" xs="12" sm="12" md="3" lg="3" xl="3">
+            <v-card :style="{ height: cardHeight + 'px' }">
+              <v-card-title>Identify target and patient population of your drug</v-card-title>
+              <v-card-text>
+              Confirm hypothesis and establish biomarkers of sensitivity and resistance
+              </v-card-text>
+              <v-card-actions>
+                <v-btn class="text-subtitle-1" color="primary" variant="outlined" rounded>
+                  Learn more 
+                  <v-icon>mdi-arrow-right-thin</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+            </v-col>
+
+            <v-col cols="auto" xs="12" sm="12" md="3" lg="3" xl="3">
+            <v-card :style="{ height: cardHeight + 'px' }">
+              <v-card-title>Explore your data on the PRISM Portal</v-card-title>
+              <v-card-text>
+              Securely browse and discover insights from your screening results with interactive visualizations
+              </v-card-text>
+              <v-card-actions>
+                <v-btn class="text-subtitle-1" color="primary" variant="outlined" rounded>
+                  Learn more 
+                  <v-icon>mdi-arrow-right-thin</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+            </v-col>
+
+        </v-row>
+        
+      </v-container>
+    </div>
+
+
+  </div>
 </template>
 <script>
   export default {
@@ -24,28 +96,42 @@
         return this.mobile ? '64px' : '128px'
       },
       heroHeight () {
-        return this.mobile ? 'calc(100vh - 64px)' : 'calc(100vh - 128px)'
+        return this.mobile ? 'calc(90vh - 64px)' : 'calc(90vh - 128px)'
+      },
+      cardHeight () {
+        return 500;
+     //   return this.mobile ? 200 : 300
+        // let heights = Array.from(document.querySelectorAll('.v-card')).map(card => card.clientHeight);
+        // return Math.max(...heights);
       }
     }
   }
 </script>
 
 <style scoped>
-.bg {
+.bg-hero {
     width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
     background: url( '../assets/hero-desktop.png') no-repeat center center;
     background-size: cover;
-    transform: scale(1);
+  }
+  .bg-gradient {
+    width: 100%;
+    min-height: 70vh;
+    background: linear-gradient(300deg, rgba(190, 219, 255, .02), rgba(190, 219, 255, .5));
   }
   .container-custom{
     width:80%;
+    padding-top:64px;
+    padding-bottom:64px;
   }
 
 
-
+.v-card > * {
+  text-overflow: visible;
+  white-space: normal;
+  word-break: normal;
+  word-wrap: initial;
+  hyphens: none;
+}
 
 </style>
