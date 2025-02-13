e2ec7c0dd039ca26e5890dc5e9a497948bf91a6c
Lia Petronio <lpetroni@broadinstitute.org>
2025-02-13 12:56:40 -0500

    standardize all pages title dom structure. clean up background page spacing

--- src/pages/research/white-papers.vue
+++ src/pages/research/white-papers.vue
@@ -1,9 +1,68 @@
 <template>
-    <v-container>
-      <h1 class="text-h1 title font-weight-bold">White papers</h1>
-    </v-container>
-</template>
+  <div>
+  <v-container>
+    <h1 class="text-h1 title font-weight-bold">White papers</h1>
+    <!-- <section>
+        <div v-for="each in whitePapers.filter((item) => item.rank == 1)">
+          <PublicationCard 
+            :title="each.title" 
+            :date="each.date" 
+            :author="`${each.author}, et al.`" 
+            :url="each.url" 
+            :abstract="each.abstract" 
+            :image="`${imgPath}${each.image}`">
+          </PublicationCard>
+        </div>
+    </section> -->
 
-<script setup>
-  //
-</script>
+    </v-container>
+
+
+
+  </div>
+  </template>
+  
+  <script>
+  // import * as d3 from 'd3';
+  // const dataPath = import.meta.env.PROD ? import.meta.env.BASE_URL+"data/" : "../../public/data/";
+  // const dataFile = "Website Content - 2025  - Publications.csv";
+
+
+    export default {
+      data() {
+        return {
+
+        }
+      },
+      async mounted() {
+       // await this.getData();
+      },
+      computed: {
+        // imgPath() {
+        //   return import.meta.env.PROD ? import.meta.env.BASE_URL + "images/publications/" : "../../public/images/publications/"
+        // },
+      },
+      methods: {
+        // async getData(){
+        //   const self = this;
+        //   Promise.all([
+        //     d3.csv(`${dataPath}${dataFile}`, function(d){
+        //         return {
+                  
+        //         }
+        //     })
+        //   ]).then(response=>{
+          
+        //   });
+        // }
+      },
+      watch: {
+
+      }
+    }
+  </script>
+
+  <style scoped>
+
+</style>
+  
