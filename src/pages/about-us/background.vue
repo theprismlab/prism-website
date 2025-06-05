ad843753be5cf7bdf8c1cbc54edb8313a6663e0b
Lia Petronio <lpetroni@broadinstitute.org>
2025-06-05 11:21:12 -0400

    delete ai and old image files

--- src/pages/about-us/background.vue
+++ src/pages/about-us/background.vue
@@ -1,26 +1,17 @@
 <template>
   <page>
     <container-sm>
-      <page-title>Background</page-title>
+      <page-title>About PRISM</page-title>
       <section>
         <p class="text-body-1">
-        Developed by the Broad Institute of MIT and Harvard, PRISM (Profiling Relative Inhibition Simultaneously in Mixtures) is a novel DNA barcoding technology that allows for rapid, viability screening of cancer cell line models in mixtures. Our original efforts have been published by Channing Yu in 2016, that showed that mixing barcoded cell lines together is feasible and by Steven Corsello in 2020 showing that screening a large number of known drugs across over 500 barcoded cell lines identified new indications for non-oncology drugs. 
-        <br><br>
-        First publication of the PRISM method from 2016. 102 cell lines were labeled with unique 24-nucleotide barcodes and used to screen 8400 compounds.   
-        <icon-link
-          href="https://www.nature.com/articles/nbt.3460"
-          text="Read our publication"
-       />    
-        <br><br>
-        An updated panel of 578 barcoded cancer cell lines were used to profile the activity of 4518 existing drugs. Results were used to begin the PRISM drug repurposing resource.
-      
-        <icon-link
-          href="https://www.nature.com/articles/s43018-019-0018-6"
-          text="Read our publication"
-        />
-        <br><br>
-        Traditional cell line viability screening is done one cell line at a time, requiring lots of time and reagents, which can slow down drug discovery and development efforts. PRISM’s unique approach to barcoding and pooling cell lines for high-throughput screening overcomes the limitations of single cell line studies, helping researchers rapidly collect cancer cell line drug-sensitivity data on an unprecedented scale. 
-      </p>
+       
+       Developed by the Broad Institute of MIT and Harvard, PRISM (Profiling Relative Inhibition Simultaneously in Mixtures) is a novel DNA barcoding technology that allows for rapid, viability screening of cancer cell line models in mixtures. Our original efforts have been published by Channing Yu in 2016, which showed that mixing barcoded cell lines together and adding drugs to the pools showed the same drug response as screening in individual cell lines. In 2020, Steven Corsello showed that screening 5,000 known drugs across over 500 barcoded cell lines confirmed the expected drug response and also identified new indications for non-oncology drugs.
+<br><br>
+The true power of PRISM is using genomically characterized cell lines that have also been profiled in the Dependency Map with CRISPR genome-wide knockout screening. Correlating the PRISM viability profile to the baseline genomic features and functional genetic readouts identifies the target, patient population, and off-target effects of drugs. 
+ <br><br>
+Since 2018, PRISM has been working with external collaborators, when we first offered PRISM to a biotech company. Now,  PRISM has worked with dozens of pharmaceutical and biotech companies and academics to uncover the true mechanism of action of their drugs. PRISM has helped identify new patient populations for clinical trials, our data has been used in Investigational New Drug (IND) filings, and we have been a part of over 40 publications. 
+
+        </p>
     </section>  
   </container-sm>
 </page>
