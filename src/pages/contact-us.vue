2302d8d319e947c0972fcc2200377a19717f53ae
liapetronio <51209514+lpetronio@users.noreply.github.com>
2025-06-06 13:45:09 -0400

    fix contact us page

--- src/pages/contact-us.vue
+++ src/pages/contact-us.vue
@@ -4,31 +4,15 @@
     <v-row class="justify-center align-center">
       <v-col cols="12" xs="12" sm="12" md="8" lg="8" xl="8">
         <page-title>Contact us</page-title>
-        <p class="text-body-1">The Broad Institute and PRISM’s mission is to help improve cancer patients’ lives. To that end, we are excited to collaborate with you to advance the understanding of cancer therapeutics and accelerate the drug discovery process. Our working model is that of a scientific collaboration to further cancer research.   
-        </p>
+     <section>
+      <h2 class="text-43">Want to collaborate?</h2>
+          <p class="text-body-1">Tell us more about your project or inquiries using form below.</p>
+          <hubspot-form></hubspot-form>
+     </section>
       </v-col>
     </v-row>
     </v-container> 
-    <gradient-dark class="py-6 my-6">
-      <v-container>
-        <v-row class="justify-center align-center">
-          <v-col cols="auto" class="text-center">
-            <section-overline color="primary-accent-1">Key collaboration points</section-overline>
-          </v-col>
-        </v-row>
-        <collaborate-cards></collaborate-cards>
-      </v-container>
-    </gradient-dark>
 
-    <v-container class="py-6 my-6">
-      <v-row class="justify-center align-center">
-        <v-col cols="12" xs="12" sm="12" md="8" lg="8" xl="8">
-          <h2 class="text-h3">Want to collaborate?</h2>
-          <p class="text-body-1">Tell us more about your project or inquiries using form below.</p>
-          <hubspot-form></hubspot-form>
-      </v-col>
-    </v-row>
-  </v-container>
 </page>
 
 </template>
