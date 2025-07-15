40fe920f4aead7e39b4f9546621bcecf7d77e1a7
Lia Petronio <lpetroni@broadinstitute.org>
2025-07-15 16:15:44 -0400

    tweak wave

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
   <page class="mt-0">
     <section style="position: relative; height: 75vh; width: 100%;">
 
-      <ThreeJs_Heatmap_Circles :key="resizeCounter"/>
+      <ThreeJs_Heatmap_Circles03 :key="resizeCounter"/>
 
       <div style="position:absolute; width: 100%; height:100%;">
         <v-container class="fill-height">
