de8e301285b23dc64533c987a95a5754f8eb3fc7
Lia Petronio <lpetroni@broadinstitute.org>
2025-07-22 15:35:39 -0400

    main graphic updated

--- src/components/sections/MainVisual.vue
+++ src/components/sections/MainVisual.vue
@@ -1,34 +1,24 @@
 <template>
     <v-container>
-        <v-row class="justify-center">  
-          <v-col cols="12" xs="12" sm="12" md="12" lg="11" xl="10" offset-sm="0" offset-md="0" offset-lg="0" offset-xl="0">
-            <v-row>
-              <v-col class="px-6">
-                <v-img :src="`${imgPath}main graphic copy 11.png`"></v-img>
-                <h3 class="text-h5 font-weight-bold">900+ genomically diverse barcoded and pooled cell lines</h3>
-                <p class="text-body-2">
-                  Hematopoietic and solid tumor, covering 80 cancer subtypes<br>
-                    <v-btn variant="text" color="primary-base">Learn more</v-btn>
-                </p>
-              </v-col>
-              <v-col class="px-6">
-                <v-img :src="`${imgPath}main graphic copy 12.png`"></v-img>
-                <h3 class="text-h5 font-weight-bold">Over 150K unique baseline genetic and functional genomic features </h3> 
-                <p class="text-body-2">
-                  Features from the Dependency Map are correlated with the PRISM viability profile using univariate and multivariate predictive models
-                </p>
-                <v-btn variant="text" color="primary-base">Learn more</v-btn>
-              </v-col>
-              <v-col class="px-6">
-                <v-img :src="`${imgPath}main graphic copy 13.png`"></v-img>
-                <h3 class="text-h5 font-weight-bold">Identify target and patient population of your drug</h3>
-                <p class="text-body-2">Confirm hypothesis and establish biomarkers of sensitivity and resistance</p>
-                <v-btn variant="text" color="primary-base">Learn more</v-btn>
+            <v-row class="justify-center">
+              <v-col class="px-0 justify-center" v-for="(card, index) in cards" :key="index" cols="12" xs="12" sm="12" md="4" lg="4">
+                <!-- <v-card class="fill-height" elevation="3"> -->
+                  <v-row no-gutters>
+                    <v-col class="px-0 justify-center" cols="5" xs="5" sm="5" md="12" lg="12" xl="12">
+                        <v-img :src="card.img" width="100%"></v-img>
+                    </v-col>
+
+                    <v-col>
+                      <v-card-item>
+                      <h3 class="text-h4">{{ card.title }}</h3>
+                      <p class="text-body-2">{{ card.description }}</p>
+                      <v-btn variant="outlined" rounded color="primary-base" :href="card.action.url" :target="card.action.target">{{ card.action.text }}<v-icon right>mdi-chevron-right</v-icon></v-btn>
+                      </v-card-item>
+                    </v-col>
+                  </v-row>         
+                <!-- </v-card> -->
               </v-col>
             </v-row>
-         
-          </v-col>
-        </v-row>
     </v-container>
 </template>
 <script>
@@ -37,12 +27,49 @@
 export default {
     name: "MainVisual",
     data(){
+      return {
+
+      }
 
     },
     computed: {
         imgPath() {
-          return import.meta.env.PROD ? import.meta.env.BASE_URL + "images/home/" : "../../public/images/home/"
+          return import.meta.env.PROD ? import.meta.env.BASE_URL + "images/" : "../../public/images/"
         },
+        cards() {
+          return [
+          {
+            img: `${this.imgPath}main-11.png`,
+            title: "900+ genomically diverse barcoded and pooled cell lines",
+            description: "Hematopoietic and solid tumor, covering 80 cancer subtypes",
+            action: {
+              text: 'Learn more',
+              url: '/consortium-screens/cell-line-collection',
+              target: '_self'
+            }
+          },
+          {
+            img: `${this.imgPath}main-12.png`,
+            title: "Over 150K unique baseline genetic and functional genomic features",
+            description: "Features from the Dependency Map are correlated with the PRISM viability profile using univariate and multivariate predictive models",
+             action: {
+              text: 'Learn more',
+              url: '/consortium-screens/data-analysis',
+              target: '_self'
+            }
+          },
+          {
+            img: `${this.imgPath}main-13.png`,
+            title: "Identify target and patient population of your drug",
+            description: "Confirm hypothesis and establish biomarkers of sensitivity and resistance",
+            action: {
+              text: 'Learn more',
+              url: '/consortium-screens/data-analysis',
+              target: '_self'
+            }
+          },
+        ]
+        }
     },
     mounted(){
 
@@ -53,18 +80,14 @@ export default {
 p{
     max-width: 500px;
 }
+.text-h4 {
+    line-height: 1.4em;
+    font-weight:500;
+}
+.text-body-2{
+  color: var(--v-grey-darken-1)
+}
 
-.v-card-title{
-    min-height:5rem !important;
-    line-height:1.33em !important;
-}
-.card-image{
-    margin:auto;
-    /* align-self: center; */
-    width:80% !important;
-    max-width: 250px !important;
-    min-width:175px !important;
-}
 /* xxl  */
 @media (min-width:  2560px) {
 
