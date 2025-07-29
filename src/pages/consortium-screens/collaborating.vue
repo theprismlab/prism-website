6544787960c3dab29997a735979cd27e50d2a063
Lia Petronio <lpetroni@broadinstitute.org>
2025-07-29 10:29:08 -0400

    collaborate page text update

--- src/pages/consortium-screens/collaborating.vue
+++ src/pages/consortium-screens/collaborating.vue
@@ -4,7 +4,7 @@
     <section>
       <page-title>Collaborate with us!</page-title>
         <p class="text-body-1">
-          If you have a test agent you want to screen in PRISM, participate in one of our PRISM Consortium Screens. These screens are run three times a year (Q1, Q2, and Q3), with specific dates available on the <a href="https://www.theprismlab.org/portal/submissions" target="_blank" class="text-link">PRISM Submission Hub</a>. Data for each screen is returned in approximately 4 months.
+          If you have a test agent you want to screen in PRISM, participate in one of our <span class="text-emphasize">PRISM Consortium Screens</span>. These screens are run three times a year (Q1, Q2, and Q3), with specific dates available on the <a href="https://www.theprismlab.org/portal/submissions" target="_blank" class="text-link">PRISM Submission Hub</a>. Data for each screen is returned in approximately 4 months.
           <br><br>
           <a href="/contact-us">Contact us</a> to receive updates on our screen offerings and submission windows!        
         </p>
@@ -28,22 +28,14 @@
         <v-row>
           <v-col cols="12" xs="12" sm="12" md="8">
             <collaborate-cards></collaborate-cards>
-          <!-- <v-card elevation="0" variant="tonal"  color="teal-cyan-lighten-4" class="pa-5 pb-7">  
-              <div v-for="card in agreements" :key="card.title">
-                <v-card-title class="d-flex align-center justify-start text-body-2 text-teal-cyan-darken-1">
-                  <svg-icon :path="card.path"  type="mdi" :size="card.size"  class="mr-2"></svg-icon>
-                {{card.title}}
-                </v-card-title>
-                <v-card-subtitle class="text-grey-darken-4 mb-2" style="padding-left: 42px">{{card.text}}</v-card-subtitle>
-              </div>
-        </v-card> -->
       </v-col>
     </v-row>
+    <p class="text-body-1">Please <a href="/contact-us">contact us</a> to receive additional information about our agreements and pricing.</p>
   </section>
    <section class="mt-12 mb-12">
       <h2 class="text-h3">Academic Collaborators</h2>
       <p class="text-body-1">
-      Collaboration agreements are not a requirement for academic collaborators to participate in our screens. If you would like to sign an agreement, please reach out to our team using the form below to receive additional information.
+        Collaboration agreements are not a requirement for academic collaborators to participate in our screens. If you would like to sign an agreement, please reach out to our team using the form below to receive additional information.
       </p>
    </section>
 </container-xs>
@@ -66,7 +58,7 @@ import { mdiLightbulbOnOutline } from '@mdi/js';
        steps: [
         'Complete the forms on the submission hub during our 2-3 week submission window <a href="https://www.theprismlab.org/portal/submissions" target="_blank" class="text-link">PRISM Submission Hub</a>.',
         'Approve and fund a quote for your PRISM submission',
-        'Ship a stock solution of your test agent(s) to the PRISM lab before the submission window deadline',
+        'Ship a stock solution of your test agent(s) to the PRISM lab before the submission window deadline!',
         'Data will be released on the PRISM Portal approximately 4 months after the submission window closes'
        ],
        agreements:  [
