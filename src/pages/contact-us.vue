ee6e6daedc91f744e912321030e95709a38634e7
Lia Petronio <lpetroni@broadinstitute.org>
2025-08-21 15:09:33 -0400

    hubspot new form

--- src/pages/contact-us.vue
+++ src/pages/contact-us.vue
@@ -3,8 +3,8 @@
 <container-xs>
     <page-title>Contact us</page-title>
      <section>
-      <h2 class="text-43">Want to collaborate?</h2>
-          <p class="text-body-1">Tell us more about your project or inquiries using form below.</p>
+      <!-- <h2 class="text-43">Want to collaborate?</h2>
+          <p class="text-body-1">Tell us more about your project or inquiries using form below.</p> -->
           <hubspot-form></hubspot-form>
      </section>
 </container-xs>
