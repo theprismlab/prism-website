5c95a5ea701cadc4e1dacb6a402ace70e6eb6ce5
liapetronio <lpetroni@broadinstitute.org>
2025-08-25 09:40:17 -0400

    banner color and font styling. close on contact us page

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -2,11 +2,9 @@
  <v-banner
    v-if="showBanner"
     :sticky="true"
-    lines="three"
-   
     closable
-        
-    class="banner px-12 pt-12 pb-4"
+    class="banner px-10 py-1"
+    color="grey lighten-4"
     >
     <!-- <v-expansion-panels>
         <v-expansion-panel>
@@ -30,26 +28,18 @@
         <p class="text-body-2">    PRISM is accepting DMSO-soluble single agents and combinations, and aqueous single agents, for our standard 5-day assay from Monday, August 25 - Friday, September 12. 
         </p>
     </v-banner-text> -->
- <v-row justify="space-between" align="center" class="mx-0 py-4">
+ <v-row justify="space-between" align="top" class="mx-0 py-2">
     <v-col cols="11">
-        <h4 class="text-h6">PRISM Submission Window Now Open!</h4>
-        <p class="text-body-2 inline-block">PRISM is accepting DMSO-soluble single agents and combinations, and aqueous single agents, for our standard 5-day assay from <b>Monday, August 25 - Friday, September 12</b>. 
-            <v-btn
-            color="primary-base"
-            to="/contact-us"
-            variant="text"
-            rounded>
-            Learn More <v-icon right>mdi-chevron-right</v-icon>
-        </v-btn>
+        <h4 class="text-body-2 font-weight-bold text-primary-base">PRISM Submission Window Now Open!</h4>
+        <p class="text-body-2 inline-block mb-0 mt-1 banner-text-body">PRISM is accepting DMSO-soluble single agents and combinations, and aqueous single agents, for our standard 5-day assay from <b>Monday, August 25 - Friday, September 12</b>. 
+        <a class="v-btn v-btn--size-default pa-0 px-4 font-weight-bold" href="/contact-us" target="_self">Learn More <v-icon right>mdi-chevron-right</v-icon></a>
         </p>
-
-
-            </v-col>
-            <v-col cols="1">
-                <v-btn icon @click="showBanner = false">
-                    <v-icon>mdi-close</v-icon>
-                </v-btn>
-            </v-col>
+    </v-col>
+    <v-col cols="1">
+        <v-btn style="background: none;" size="x-small" icon @click="showBanner = false" flat>
+            <v-icon right>mdi-close</v-icon>
+        </v-btn>
+    </v-col>
  </v-row>
    
   
@@ -73,24 +63,40 @@ export default {
             panel: [0],
         }
     },
-    methods: {
-
+    methods: {},
+    computed: {},
+    watch: {
+        // Watch for route changes
+        $route(to) {
+            if (to.path === '/contact-us') {
+                this.showBanner = false;
+            } else {
+                this.showBanner = true;
+            }
+        },
     },
-    computed: {
-
-
+    mounted() {
+        // Check the route when the component is mounted
+        if (this.$route.path === '/contact-us') {
+            this.showBanner = false;
+        }
     },
-    watch:{
-
-    }
 }
 </script>
 <style scoped>
 .v-banner{
     padding-inline: 0px 0px;
-    padding-top: 0px !important;
-    padding-bottom: 0px !important;
-    
+    /* padding-top: 0px !important;
+    padding-bottom: 0px !important; */
+    /* background-color: var(--v-primary-lighten-6) !important; */
+    border-bottom: 1px solid var(--v-divider-base) !important;
+    /* background: linear-gradient(120deg, var(--v-primary-lighten-6), var(--v-primary-accent-1));  */
+    /* background: linear-gradient(120deg, var(--v-primary-accent-1-lighten-1), var(--v-primary-accent-1-lighten-2) ); */
+    background: linear-gradient(120deg, var(--v-primary-lighten-6), var(--v-primary-base-lighten-5) );
+}
+.banner-text-body{
+    color: var(--v-grey-darken-3);
+    font-size: 0.85rem !important;
 }
 .banner{
     top: var(--v-toolbar-height);
