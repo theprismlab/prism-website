b51097eb688ff1302b2a58ae14fce6617ee67ab1
Lia Petronio <lpetroni@broadinstitute.org>
2026-03-24 15:25:11 -0400

    new banner for aacr

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,15 +2,21 @@
   <AppBar/>
   <v-main>
       
-       <app-banner v-if="showBanner"></app-banner>
+       <!-- <app-banner v-if="showBanner"></app-banner> -->
+               <AACRBanner v-if="showBanner"></AACRBanner>
     <router-view />
   </v-main>
   <AppFooter />
 </template>
 
 <script>
+import AACRBanner from '@/components/AACRBanner.vue';
+
 export default {
   name: 'Default',
+  components: {
+    AACRBanner
+  },
   data: function() {
     return {
       showBanner: true
