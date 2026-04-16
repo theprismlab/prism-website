43545328d7a04bfe52ee0a6430fa302196c5646a
Lia Petronio <lpetroni@broadinstitute.org>
2026-04-16 10:26:50 -0400

    publication parsing and display

--- src/pages/publications.vue
+++ src/pages/publications.vue
@@ -36,11 +36,30 @@
               :color="typeStyles[each.type].color"
               border="start"
               colored-border
-              variant="tonal"
+              variant="outlined"
               class="mb-3"
+    
             >
-                {{  each.title }}
-              <v-divider></v-divider>
+            <v-alert-title class="publication-title">
+              
+              <a :href="each.link" target="_blank" style="color: inherit; text-decoration: none;">
+                {{ each.title }} <v-icon right size="x-small" class="pl-1">mdi-open-in-new</v-icon>
+              </a>
+
+            </v-alert-title>
+            <div class="publication-meta">
+             <span v-if="each.author">{{ each.author }}, et al. </span>
+            <span v-if="each.publisher">{{ each.publisher || each.conference }}, </span>
+            <span>{{ each.date || each.year }}</span>.
+            </div>
+
+           
+
+  
+
+          
+
+           
             </v-alert>
             
           </v-col>
@@ -51,8 +70,20 @@
   </template>
   
   <script>
+  import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiBookMultipleOutline } from '@mdi/js';
+  import { mdiBookOpenBlankVariantOutline } from '@mdi/js';
+import { mdiFileCertificateOutline } from '@mdi/js';
+import { mdiFileChartOutline } from '@mdi/js';
+import { mdiFileDocumentMultipleOutline } from '@mdi/js';
+import { mdiFileMultipleOutline } from '@mdi/js';
+import { mdiFilePresentationBox } from '@mdi/js';
+import { mdiPresentation } from '@mdi/js';
+
+
   import * as d3 from 'd3';
   import CrossfilterManager from '@/utils/crossfilter-helpers.js';
+import svgIconVue from '@jamescoyle/vue-icon';
   const dataPath = import.meta.env.PROD ? import.meta.env.BASE_URL+"data/" : "../public/data/";
   const publicationsFile = "Website Content - 2025  - Publications.csv";
   const whitepaperFile = "Website Content - 2025  - White Papers.csv";
@@ -86,9 +117,20 @@
         },
         typeStyles() {
           return {
-            publication: { icon: 'mdi-file-document-outline', color: 'blue' },
-            whitepaper: { icon: 'mdi-note-outline', color: 'teal' },
-            conferenceAbstract: { icon: 'mdi-microphone-outline', color: 'deep-purple' },
+            "publication": { icon: 'mdi-file-document-outline', color: 'indigo-accent-3' }, // blue
+            // "white paper": { icon: 'mdi-note-outline', color: 'teal' },
+                      "white paper": { icon: 'mdi-book-open-blank-variant-outline', color: 'teal' },
+            "conference abstract": { icon: 'mdi-presentation', color: 'purple' }, // deep-purple
+            //mdi-file-chart-outline
+            // mdi-chart-box-outline
+            //mdi-chart-box-multiple-outline
+            //mdi-note-multiple-outline
+            //mdi-newspaper-variant-outline
+            //mdi-note-text-outline
+            // mdi-format-quote-open
+            //mdi-file-delimited-outline
+            //mdi-file-certificate-outline
+            //mdi-file-star-outline
           }
         },
       },
@@ -101,24 +143,32 @@
                 return {
                     title: d.Title,
                     type: "publication",
-                    id: `publication-${i}`
-                  //  date: d.year
+                    id: `publication-${i}`,
+                    year: d.Year,
+                    link: d.Link,
+                    publisher: d.Publication,
+                    author: d.Author
                 }
             }),
             d3.csv(`${dataPath}${whitepaperFile}`, function(d, i){
                 return {
                     title: d.Title,
-                    type: "whitepaper",
-                    id: `whitepaper-${i}`
-                 //   date: d.year
+                    type: "white paper",
+                    id: `white-paper-${i}`,
+                    link: d.Link,
+                    portalLink: d["Portal Link"],
+                    date: d.Date,
+                    year: d.Date.split(" ")[2],
+                    conference: d.Conference
                 }
             }),
             d3.csv(`${dataPath}${conferenceAbstractsFile}`, function(d, i){
                 return {
                     title: d.Title,
-                    type: "conferenceAbstract",
-                    id: `conferenceAbstracts-${i}`,
-                    //   date: d.year
+                    type: "conference abstract",
+                    id: `conference-abstract-${i}`,
+                    year: d.Year,
+                    link: d.Link,
                 }
             })
           ]
@@ -145,6 +195,20 @@
   </script>
 
   <style scoped>
+.publication-title {
+  margin: 0;
+  font-size: 1.25rem;
+  font-weight: 500;
+  color: rgb(23, 23, 23) !important;
+}
+.publication-meta {
+  font-size: 0.875rem;
+  color: rgb(97, 97, 97);
+  margin-top: 4px;
+}
+.v-alert--variant-outlined {
+    border: 0.1px solid currentColor;
+}
 
 </style>
   
