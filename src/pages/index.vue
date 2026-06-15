f7419f9a336fe099089f6198310dfd48b8bf0a8a
Lia Petronio <lpetroni@broadinstitute.org>
2026-06-15 08:29:00 -0400

    portal section fix

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,19 +3,23 @@
     <HomeHero></HomeHero>
     <page-section background="tinted" :padding="10">
       <app-container>
-        <p class="prism-text-h3 text-center mb-0">
-          Our lab partners with researchers to <span class="text-gradient-blue">reveal clinically important differences in drug
-          behavior</span> by screening over 900 cancer cell lines and correlating the viability results with
+        <p class="prism-text-h3 text-center mb-0" style="line-height: 2.4rem">
+          Our lab partners with researchers to
+          <span class="text-gradient-blue"
+            >reveal clinically important differences in drug behavior</span
+          >
+          by screening over 900 cancer cell lines and correlating the viability results with
           baseline and functional genomic features
         </p>
       </app-container>
     </page-section>
+
     <app-container>
       <p class="prism-text-body-1">
         Cancer cell line models remain essential in drug development, providing a controlled system
         to assess potency and confirm on-target engagement. At a larger scale using PRISM, these
-        models provide power to <span class="text-gradient-blue">understand heterogeneity</span> in drug
-        response among cell lines harboring the targeted alteration(s), assess
+        models provide power to <span class="text-gradient-blue">understand heterogeneity</span> in
+        drug response among cell lines harboring the targeted alteration(s), assess
         <span class="text-gradient-blue"
           >selectivity relative to those without target alterations, and investigate potential
           biomarkers associated with these responses</span
@@ -23,21 +27,21 @@
       </p>
     </app-container>
 
-    
-      <app-container wide class="gradient-background">
-        <HomeOverviewCards></HomeOverviewCards>
-      </app-container>
-
+    <app-container wide class="gradient-background">
+      <HomeOverviewCards></HomeOverviewCards>
+    </app-container>
 
     <page-section background="gradient" :padding="8">
       <app-container wide>
-        <our-portal></our-portal>
+        <HomePortal></HomePortal>
       </app-container>
     </page-section>
 
-    <app-container class="my-12">
+    <app-container class="section">
       <section-overline class="text-center">Impact</section-overline>
-      <prism-section-title class="text-center">Dedicated to advancing oncology research</prism-section-title>
+      <prism-section-title class="text-center"
+        >Dedicated to advancing oncology research</prism-section-title
+      >
       <p class="prism-text-body-1 text-center">
         Working with pharmaceutical and biotech companies and academics to improve the outcome for
         cancer patients worldwide.
@@ -45,7 +49,7 @@
       <HomeImpactCards></HomeImpactCards>
     </app-container>
 
-    <app-container class="my-12">
+    <app-container class="section">
       <section-overline class="text-center">Testimonials</section-overline>
       <HomeTestimonialCards></HomeTestimonialCards>
     </app-container>
@@ -53,18 +57,17 @@
 </template>
 <script>
   import SvgIcon from '@jamescoyle/vue-icon';
-  import { mdiHexagonMultiple } from '@mdi/js';
-  import { mdiFileDocumentMultipleOutline } from '@mdi/js';
-  import { mdiAccountGroup } from '@mdi/js';
   import HomeHero from '@/pages/index/HomeHero.vue';
   import { ASSET_BASE } from '@/utils/assets';
   import HomeOverviewCards from '@/pages/index/HomeOverviewCards.vue';
   import HomeTestimonialCards from '@/pages/index/HomeTestimonialCards.vue';
   import HomeImpactCards from '@/pages/index/HomeImpactCards.vue';
+  import HomePortal from '@/pages/index/HomePortal.vue';
   export default {
     components: {
       SvgIcon,
       HomeHero,
+      HomePortal,
       HomeOverviewCards,
       HomeTestimonialCards,
       HomeImpactCards,
@@ -105,9 +108,15 @@
 </script>
 
 <style scoped lang="scss">
-.gradient-background {
-  background: linear-gradient(180deg, #D4EAF6, #F9F9F9 75%, #FFF);
-}
+  .section {
+    margin-top: 64px;
+    margin-bottom: 64px;
+    padding-top: 24px;
+    padding-bottom: 24px;
+  }
+  .gradient-background {
+    background: linear-gradient(180deg, #d4eaf6, #f9f9f9 75%, #fff);
+  }
   .hero-section {
     display: grid;
     height: calc(100dvh - var(--v-layout-top, 64px));
